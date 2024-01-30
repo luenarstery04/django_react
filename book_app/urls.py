2fc48f6e9280a9876abdc3b8c6eb103c64ae3527
LuenArstery04 <csh98.dev@gmail.com>
2024-01-30 10:01:37 +0900

    django-ract project

---
+++ book_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import helloAPI, BooksAPIMixins, BookAPIMixins, BookSearchList, BooksChart, file_upload_one
+
+urlpatterns = [
+    path("hello/", helloAPI),
+    path("books/", BooksAPIMixins.as_view()),
+    path("book/<str:bookno>/", BookAPIMixins.as_view()),
+    path("search/<str:type>/<str:keyword>/", BookSearchList.as_view()),
+    path("chart_db/", BooksChart.as_view()),
+    path("upload/", file_upload_one),
+]
